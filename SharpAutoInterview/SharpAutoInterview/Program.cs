2d565c5892195ffb300048909c65b3052da1307e
Katya Nikitina <katyanikitina@coherentsolutions.com>
2022-09-20 14:22:13 +0200

    created BubbleSort class

--- SharpAutoInterview/SharpAutoInterview/Program.cs
+++ SharpAutoInterview/SharpAutoInterview/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// using System;
+//
+// namespace SharpAutoInterview 
+// {
+//     internal static class Program
+//     {
+//         static void Main(string[] args)
+//         {
 
-Console.WriteLine("Hello, World!");
+using System;
+using SharpAutoInterview;
+
+Console.WriteLine("Bubble Sort");
+Console.Write("Enter array elements : "); 
+var parts = Console.ReadLine()?.Split(new[] { " ", ",", ";" }, StringSplitOptions.RemoveEmptyEntries); 
+
+if (parts != null) 
+{ 
+    var array = new int[parts.Length]; 
+    for (int i = 0; i < parts.Length; i++) 
+    { 
+        array[i] = Convert.ToInt32(parts[i]);
+    }
+    
+    Console.WriteLine("Bubble Sorted Array: {0}", string.Join(", ", BubbleSort.SortBubble(array)));
+}
+
+Console.ReadLine();   
+    //    }
+  //  }
+//}
+
+
+
